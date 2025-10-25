077e38c4d606f94c1fedf6c88469325bf4f59f74
jean ronald <jeroboumg@gmail.com>
2025-10-25 16:56:36 +0100

    mise en place des secret env

--- services/sendEmail.ts
+++ services/sendEmail.ts
@@ -1,11 +1,11 @@
 import { sendForm } from 'emailjs-com';
 
-const SERVICE_ID = 'service_88kf7ji';
-const TEMPLATE_ID = 'template_rqlaomo';
-const USER_ID = 'ngFjEYY9BamsPuTcB';
+const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+const USER_ID = process.env.NEXT_PUBLIC_EMAILJS_USER_ID!;
 
 const sendEmail = async (e: HTMLFormElement) => {
-	e.preventDefault;
+	e.preventDefault();
 	try {
 		const response = await sendForm(SERVICE_ID, TEMPLATE_ID, e, USER_ID);
 		return response.status;
